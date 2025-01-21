e64a6b7c052de943cbd2e168bf04d15bf36f7c8c
simon970 <107906302+simon970@users.noreply.github.com>
2025-01-21 00:13:38 -0600

    completed Chat

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -109,4 +109,22 @@ app.post("/room",middleware,async (req,res)=>{
  
 })
 
+app.get("/chat/:roomId",async (req,res)=>{
+    const roomId= Number(req.params.roomId);
+    const messages = await prismaClient.chat.findMany({
+        where:{
+            roomId:roomId
+        },
+        orderBy:{
+            id:"desc"
+        },
+        take:50
+
+    })
+
+    res.json({
+        messages
+    })
+})
+
 app.listen(3001);
